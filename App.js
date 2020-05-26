ffaa8af8236f1745a97bbc99c68445bb53d4bccd
melika238 <melikakouki0@gmail.com>
2020-05-26 11:30:51 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+<div style={{border:'solid' ,'1px': 'black',maxWidth:'100vw'}}>  
+
+<h1 className="titleRed">Your name here</h1>
+ 
+
+
+import imageInSrc from "./imageInSrc.jpg"
+ function app(){
+	return( <br>
+	 <img src={imageInSrc}/>
+	 </br>);
+ }
+</div>
+
+
+         <img src="/imageInPublic.jpg" /> 
+  
+<video width="320" height="240" controls>  
+<source src="myVideo.mp4" type="video/mp4"/> 
+    
+</video>
